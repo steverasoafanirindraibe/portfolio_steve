d6e204a3960f91f96b4c7e5674d6ebe130f01796
steverasoafanirindraibe <steveshannyrasoafanirindraibe@gmail.com>
2025-11-29 00:10:38 +0300

    Placement de tous les types d'icon du titre

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,31 @@ import ClientLayout from './ClientLayout';
 export const metadata: Metadata = {
   title: "Steve Rasoafanirindraibe - Portfolio",
   description: "Portfolio de Steve Rasoafanirindraibe, développeur full-stack",
+    icons: {
+    icon: [
+      {
+        url: "/favicon.ico",
+        sizes: "any",
+      },
+      {
+        url: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/favicon-32x32.png", 
+        sizes: "32x32",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      {
+        url: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
